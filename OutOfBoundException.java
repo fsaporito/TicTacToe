d8af7a51d43947d296e6d09e522e5126c1ceda25
sapo93 <sapo93@gmail.com>
2013-11-15 14:07:11 +0100

    Create OutOfBoundException.java

---
+++ OutOfBoundException.java
@@ -0,0 +1,15 @@
+public class OutOfBoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public OutOfBoundException() {
+		
+	}
+
+	public OutOfBoundException(String arg0) {
+		
+		super(arg0);
+		
+	}
+
+}
